0cc6d8c7f1dea29f99a668b5b8e63824b6df08f9
Hariom Kumar <hariom.kumar8april@gmail.com>
2025-05-03 10:36:04 +0530

    first cpp code

---
+++ array/hellowWorld.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // This is a simple C++ program that prints "Hello, World!" to the console.
+    cout << "Hello, World!" << endl;
+    
+    return 0;
+}
